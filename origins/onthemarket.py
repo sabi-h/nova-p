91254fbdab6da023aa37aeb575bbf773b80525f5
Sabih <sabih.hasan@globalservs.com>
2021-02-10 21:56:24 +0000

    adds london stations sheet

--- origins/onthemarket.py
+++ origins/onthemarket.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 
 from utils import url_to_filename, filename_to_url
@@ -5,6 +6,27 @@ from utils import url_to_filename, filename_to_url
 """
 Get list of all stations in london, loop through them.
 """
+
+stations_df = pd.read_csv('../data/misc/london-stations.csv')
+print(stations_df)
+
+
+HEADERS = {
+	'authority': 'www.onthemarket.com',
+	'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+	'accept': 'application/json',
+	'sec-ch-ua-mobile': '?0',
+	'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36',
+	'content-type': 'application/json; charset=utf-8',
+	'sec-fetch-site': 'same-origin',
+	'sec-fetch-mode': 'cors',
+	'sec-fetch-dest': 'empty',
+	'referer': 'https://www.onthemarket.com/new-homes/property/london/?view=grid',
+	'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'
+}
+
+
+
 
 def test_request():
 	import requests
