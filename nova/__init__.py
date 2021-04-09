a390d8cf113b25a99930c9626d1bad3f34a00249
Sabih <sabih.hasan@globalservs.com>
2021-04-09 08:31:30 +0100

    name fix

--- nova/__init__.py
+++ nova/__init__.py
@@ -1,2 +1,2 @@
 from nova.onthemarket.property_tiles import main as get_data_onthemarket
-from nova.landregistry.main import main as gety_data_landregistry
+from nova.landregistry.main import main as get_data_landregistry
